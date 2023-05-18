a609dc9c26ce16b34d0afff4dc8c364ac8b4eacf
Bruno Henrique <bh45237@gmail.com>
2023-05-18 16:29:03 -0300

    botao de voltar da pagina de nao encontrado atualizado

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,16 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as NotFound } from 'src/assets/icons/notFound.svg';
+import { ReactComponent as Arrow } from 'src/assets/icons/chevron.svg';
 
 import './NotFoundPage.css';
-import { Link } from 'react-router-dom';
 
 export default function NotFoundPage() {
+  const nav = useNavigate()
+
   return (
     <section className="notFoundContent">
       <h1 className="errorMessage">Erro 404 - Página não encontrada</h1>
       <NotFound className="maskImage"/>
       <div className="notFoundtextBox">
         <h1>A página que você tentou acessar não existe</h1>
-        <Link className="backLink" to="/">Voltar para o inicío</Link>
+        <button onClick={() => nav(-1)}className="backLink" to="/">
+          <Arrow className="backIcon"/>
+          Voltar
+        </button>
       </div>
     </section>
   )
